e29a3f01b7732351d6c0654735ea939035971c97
SebastianJuncosL <sebastianjuncos@gmail.com>
2021-07-06 17:14:37 -0500

    User can logout

--- app/src/main/java/com/example/myig/MainActivity.java
+++ app/src/main/java/com/example/myig/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.myig;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void onLogoutAction(MenuItem menuItem){
+        ParseUser.logOut();
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+        finish();
     }
 }
