2d956d8ab564bc3f41e16ce2f3b649b02b28b60b
SebastianJuncosL <sebastianjuncos@gmail.com>
2021-07-06 23:06:52 -0500

    Updated main activty (visual) and gets Post description and user (not visual)

--- app/src/main/java/com/example/myig/MainActivity.java
+++ app/src/main/java/com/example/myig/MainActivity.java
@@ -4,19 +4,55 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "MainActivity";
+    private EditText etDescription;
+    private Button btnTakeImage;
+    private ImageView ivPostImage;
+    private Button btnSubmit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etDescription = findViewById(R.id.etDescription);
+        btnTakeImage = findViewById(R.id.btnTakeImage);
+        ivPostImage = findViewById(R.id.ivPostImage);
+        btnSubmit = findViewById(R.id.btnSubmit);
+
+        queryPost();
     }
 
+    private void queryPost() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if(e!= null){
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                for (Post post: posts) {
+                    Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
+                }
+            }
+        });
+    }
 
 
     @Override
--- app/src/main/java/com/example/myig/ParseApplication.java
+++ app/src/main/java/com/example/myig/ParseApplication.java
@@ -3,11 +3,13 @@ package com.example.myig;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication  extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("DAMfnIW498OeO1Y7Qixr7ybB7mk1zLb5vZODPjW5")
