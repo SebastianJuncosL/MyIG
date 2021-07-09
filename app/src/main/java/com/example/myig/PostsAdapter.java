6f0112d5abf600252f333b6ed3c5b3e17935c1eb
SebastianJuncosL <sebastianjuncos@gmail.com>
2021-07-08 22:23:57 -0500

    Added timestamp to feed, still missing deatiled view for each post

--- app/src/main/java/com/example/myig/PostsAdapter.java
+++ app/src/main/java/com/example/myig/PostsAdapter.java
@@ -1,6 +1,7 @@
 package com.example.myig;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.parse.ParseFile;
 
+import java.util.Date;
 import java.util.List;
 
 public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> {
@@ -48,12 +50,16 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> 
         private TextView tvUser;
         private ImageView ivPost;
         private TextView tvDesc;
+        private TextView tvTime;
+        private Date createdAt;
+        private String timeAgo;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             tvUser = itemView.findViewById(R.id.tvUser);
             ivPost = itemView.findViewById(R.id.ivPost);
             tvDesc = itemView.findViewById(R.id.tvDesc);
+            tvTime = itemView.findViewById(R.id.tvTime);
         }
 
         public void bind(Post post) {
@@ -64,7 +70,46 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> 
             if(image != null) {
                 Glide.with(context).load(post.getImage().getUrl()).into(ivPost);
             }
+            createdAt = post.getCreatedAt();
+            timeAgo = calculateTimeAgo(createdAt);
+            tvTime.setText(timeAgo);
 
+        }
+
+        public String calculateTimeAgo(Date createdAt) {
+
+            int SECOND_MILLIS = 1000;
+            int MINUTE_MILLIS = 60 * SECOND_MILLIS;
+            int HOUR_MILLIS = 60 * MINUTE_MILLIS;
+            int DAY_MILLIS = 24 * HOUR_MILLIS;
+
+            try {
+                createdAt.getTime();
+                long time = createdAt.getTime();
+                long now = System.currentTimeMillis();
+
+                final long diff = now - time;
+                if (diff < MINUTE_MILLIS) {
+                    return "just now";
+                } else if (diff < 2 * MINUTE_MILLIS) {
+                    return "a minute ago";
+                } else if (diff < 50 * MINUTE_MILLIS) {
+                    return diff / MINUTE_MILLIS + " m ago";
+                } else if (diff < 90 * MINUTE_MILLIS) {
+                    return "an hour ago";
+                } else if (diff < 24 * HOUR_MILLIS) {
+                    return diff / HOUR_MILLIS + " h ago";
+                } else if (diff < 48 * HOUR_MILLIS) {
+                    return "yesterday";
+                } else {
+                    return diff / DAY_MILLIS + " d ago";
+                }
+            } catch (Exception e) {
+                Log.i("Error:", "getRelativeTimeAgo failed", e);
+                e.printStackTrace();
+            }
+
+            return "";
         }
     }
 }
